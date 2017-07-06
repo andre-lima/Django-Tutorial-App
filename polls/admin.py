42a73eceec955c95c9dccce35325701282e5b469
Andre-SFL <andre.dantas@savoirfairelinux.com>
2017-07-06 16:53:15 -0400

    Finished tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -8,6 +8,7 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+        fields = ['pub_date', 'question_text']
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
