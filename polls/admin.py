db9910def695c1d083414019fc80b9035fe342c9
Andre-SFL <andre.dantas@savoirfairelinux.com>
2017-07-10 10:17:43 -0400

    debug

--- polls/admin.py
+++ polls/admin.py
@@ -8,7 +8,6 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-        fields = ['pub_date', 'question_text']
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
